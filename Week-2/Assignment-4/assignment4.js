fe98e67c97d6acd014abcad1a79d693f9391435d
AmberCheng0806 <qaz29zaq34@gmail.com>
2024-06-26 00:33:03 +0800

    Create W2 folder

---
+++ Week-2/Assignment-4/assignment4.js
@@ -0,0 +1,4 @@
+window.alert("HI")
+document.getElementsByClassName("welcome").addEventListener("click",function(){
+    const textChange = document.getElementsByClassName("welcome");
+    textChange.textContent = "Have a Good Time!"
