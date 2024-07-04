aec346cb8762a7944cea29cfc04ee50f0f262c16
AmberCheng0806 <qaz29zaq34@gmail.com>
2024-07-04 20:32:20 +0800

    Week3 assignment 1st version

---
+++ Week-3/school_assignment/src/main/java/tw/idv/amber/SchoolAssignmentApplication.java
@@ -0,0 +1,13 @@
+package tw.idv.amber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolAssignmentApplication.class, args);
+	}
+
+}
---
+++ Week-3/school_assignment/src/test/java/tw/idv/amber/SchoolAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.idv.amber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
