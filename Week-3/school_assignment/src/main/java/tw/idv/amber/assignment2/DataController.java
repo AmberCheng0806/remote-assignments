bde759d10f3d84c6a19a1c456e32184954ab3b09
AmberCheng0806 <qaz29zaq34@gmail.com>
2024-07-05 00:05:17 +0800

    Week3 assignment version2

---
+++ Week-3/school_assignment/src/main/java/tw/idv/amber/assignment2/DataController.java
@@ -0,0 +1,27 @@
+package tw.idv.amber.assignment2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DataController {
+    @GetMapping("data")
+    public String data(@RequestParam(required = false) String number) {
+        if (number == null) {
+            return "Lack of Parameter";
+        } else {
+            try {
+                int i = Integer.parseInt(number);
+                int sum = 0;
+                for (int j = 1; j <= i; j++) {
+                    sum += j;
+                }
+                return String.valueOf(sum);
+            } catch (NumberFormatException e) {
+                return "Wrong Parameter";
+            }
+        }
+    }
+
+}
